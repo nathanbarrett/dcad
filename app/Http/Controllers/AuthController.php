0f5ee8ea096dca4bfeb2682898aef2469ef87025
Nathan Barrett <nathan.barrett@gmail.com>
2022-08-22 15:25:33 -0500

    forgot User class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
