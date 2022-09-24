9d6bba4435e3ffc5fa6eac569e3b056c22b2f9e9
Nathan Barrett <nathan.barrett@gmail.com>
2022-09-24 18:35:18 -0500

    add command && temp bypass migration

---
+++ app/Console/Commands/TempPopulateOwnerPropertyId.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\OwnerProperty;
+use Illuminate\Console\Command;
+
+class TempPopulateOwnerPropertyId extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dcad:tmp:update_owner_property_id';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Populates a new column in the property_changes table';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $query = PropertyChange::query()
+            ->whereNull('owner_property_id')
+            ->where('type', PropertyChange::TYPE_OWNER_UPDATE);
+
+        $bar = $this->output->createProgressBar($query->count());
+        $bar->start();
+
+        $query->chunkById(1000, function ($changes) use ($bar) {
+                /* @var PropertyChange $propertyChange */
+                foreach ($changes as $propertyChange) {
+                    if ($propertyChange->context->has('pivot_id')) {
+                        $propertyChange->owner_property_id = $propertyChange->context->get('pivot_id');
+                        $propertyChange->context = $propertyChange->context->except('pivot_id');
+                        $propertyChange->save();
+                        continue;
+                    }
+                    $pivotMatch = OwnerProperty::query()
+                        ->where('property_id', $propertyChange->property_id)
+                        ->whereBetween('created_at', [$propertyChange->created_at->subMinutes(1), $propertyChange->created_at->addMinutes(1)])
+                        ->first();
+
+                    if ($pivotMatch) {
+                        $propertyChange->owner_property_id = $pivotMatch->id;
+                        $propertyChange->save();
+                    }
+                }
+                $bar->advance(count($changes));
+            });
+
+        $bar->finish();
+
+        return self::SUCCESS;
+    }
+}
--- database/migrations/2022_09_06_223458_add_pivot_id_to_property_changes_table.php
+++ database/migrations/2022_09_06_223458_add_pivot_id_to_property_changes_table.php
@@ -16,37 +16,13 @@ return new class extends Migration
     public function up()
     {
         Schema::table('property_changes', function (Blueprint $table) {
-            $table->unsignedBigInteger('owner_property_id')
-                ->nullable()
-                ->after('property_id');
-            $table->foreign('owner_property_id')
-                ->references('id')
-                ->on('owner_property');
+//            $table->unsignedBigInteger('owner_property_id')
+//                ->nullable()
+//                ->after('property_id');
+//            $table->foreign('owner_property_id')
+//                ->references('id')
+//                ->on('owner_property');
         });
-
-        PropertyChange::query()
-            ->whereNull('owner_property_id')
-            ->where('type', PropertyChange::TYPE_OWNER_UPDATE)
-            ->chunkById(1000, function ($changes) {
-                /* @var PropertyChange $propertyChange */
-                foreach ($changes as $propertyChange) {
-                    if ($propertyChange->context->has('pivot_id')) {
-                        $propertyChange->owner_property_id = $propertyChange->context->get('pivot_id');
-                        $propertyChange->context = $propertyChange->context->except('pivot_id');
-                        $propertyChange->save();
-                        continue;
-                    }
-                    $pivotMatch = OwnerProperty::query()
-                        ->where('property_id', $propertyChange->property_id)
-                        ->whereBetween('created_at', [$propertyChange->created_at->subMinutes(1), $propertyChange->created_at->addMinutes(1)])
-                        ->first();
-
-                    if ($pivotMatch) {
-                        $propertyChange->owner_property_id = $pivotMatch->id;
-                        $propertyChange->save();
-                    }
-                }
-            });
     }
 
     /**
